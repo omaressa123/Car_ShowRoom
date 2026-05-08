6243ad65067ff6cb4a79efd04c138d437725c7a6
YahyaTaher <coolyahya2004@gmail.com>
2026-05-08 16:04:51 +0300

    nothing changed

--- src/main/resources/static/js/api.js
+++ src/main/resources/static/js/api.js
@@ -33,7 +33,7 @@ function updateNavbar() {
     }
 
     navbar.innerHTML = `
-        <a href="/" class="logo">DevOps</a>
+        <a href="/" class="logo">Car Showroom</a>
         <ul class="nav-links">
             ${navLinks}
         </ul>
